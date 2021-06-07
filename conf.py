2c7091d492ce50e7c63524647b6b563952907563
Chen Bainian <chenbn@artc.a-star.edu.sg>
2021-06-07 14:55:37 +0800

    1.0.0-rc1
    Signed-off-by: Chen Bainian <chenbn@artc.a-star.edu.sg>

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2021, ROS Industrial Asia Pacific'
 author = 'Chen Bainian'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0-beta'
+release = '1.0.0-rc1'
 
 
 # -- General configuration ---------------------------------------------------
